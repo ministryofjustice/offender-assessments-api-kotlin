845d382b8ceeaeec6f91c4d72c32a3b2b5044ad3
nataliewood <natalie.wood@digital.justice.gov.uk>
2020-08-04 12:43:59 +0100

    include ReferenceDataController

---
+++ src/main/kotlin/uk/gov/justice/digital/oasys/controllers/ReferenceDataController.kt
@@ -0,0 +1,23 @@
+package uk.gov.justice.digital.oasys.controllers
+
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.oasys.api.RefElementDto
+import uk.gov.justice.digital.oasys.services.ReferenceDataService
+
+@RestController
+@Api(description = "Reference resources", tags = ["Reference Data"])
+class ReferenceDataController( private val referenceDataService: ReferenceDataService) {
+
+    @GetMapping(path = ["/referencedata/{category}"])
+    @ApiOperation(value = "Gets reference data for a category")
+    @ApiResponses(ApiResponse(code = 200, message = "OK"))
+    fun getReferenceDataByCategoryCode(@PathVariable("category") refDataCategoryCode: String?): Collection<RefElementDto?>? {
+        return referenceDataService.getActiveReferenceDataOfCategory(refDataCategoryCode)
+    }
+}
