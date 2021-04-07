5f59127827a0d499e5f72af0164ac104616600ee
Stuart Whitlam <stuart.whitlam@digital.justice.gov.uk>
2021-04-07 07:37:17 +0100

    :sparkles: Add filtered reference data endpoint

--- src/main/kotlin/uk/gov/justice/digital/oasys/controllers/ReferenceDataController.kt
+++ src/main/kotlin/uk/gov/justice/digital/oasys/controllers/ReferenceDataController.kt
@@ -6,7 +6,10 @@ import io.swagger.annotations.ApiResponse
 import io.swagger.annotations.ApiResponses
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.oasys.api.FilteredRefDataRequestDto
 import uk.gov.justice.digital.oasys.api.RefElementDto
 import uk.gov.justice.digital.oasys.services.ReferenceDataService
 
@@ -19,5 +22,12 @@ class ReferenceDataController(private val referenceDataService: ReferenceDataSer
   @ApiResponses(ApiResponse(code = 200, message = "OK"))
   fun getReferenceDataByCategoryCode(@PathVariable("category") refDataCategoryCode: String?): Collection<RefElementDto?>? {
     return referenceDataService.getActiveReferenceDataOfCategory(refDataCategoryCode)
+  }
+
+  @PostMapping(path = ["/referencedata/filtered"])
+  @ApiOperation(value = "Gets filtered reference data", hidden = true)
+  @ApiResponses(ApiResponse(code = 200, message = "OK"))
+  fun getFilteredReferenceData(@RequestBody filteredRefDataRequest: FilteredRefDataRequestDto): Map<String, Collection<RefElementDto>> {
+    return referenceDataService.getFilteredReferenceData(filteredRefDataRequest)
   }
 }
--- src/main/kotlin/uk/gov/justice/digital/oasys/jpa/repositories/ReferenceDataRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/oasys/jpa/repositories/ReferenceDataRepository.kt
@@ -1,18 +1,101 @@
 package uk.gov.justice.digital.oasys.jpa.repositories
 
+import org.hibernate.Session
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.oasys.jpa.entities.RefElement
+import java.sql.Connection
+import java.sql.Types
 import java.time.LocalDateTime
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+
+interface ReferenceDataDao {
+  fun findFilteredReferenceData(
+    username: String,
+    area: String,
+    team: String? = null,
+    assessor: String? = null,
+    offenderPk: Long? = null,
+    oasysSetPk: Long? = null,
+    assessmentType: String? = null,
+    section: String? = null,
+    fieldName: String,
+    parentList: Map<String, String>? = emptyMap(),
+    stage: String? = "CONTINUE"
+  ): String
+}
 
 @Repository
-interface ReferenceDataRepository : JpaRepository<RefElement?, Long?> {
+interface ReferenceDataRepository : JpaRepository<RefElement?, Long?>, ReferenceDataDao {
   @Query(
     """
-            SELECT r FROM RefElement r where (r.refCategoryCode = ?1)
-            AND (?2 BETWEEN r.startDate and r.endDate)
-            """
+ SELECT r FROM RefElement r where (r.refCategoryCode = ?1)
+ AND (?2 BETWEEN r.startDate and r.endDate)
+ """
   )
-  fun findAllByRefCategoryCodeAndBetweenStartAndEndDate(refCategoryCode: String?, endDate: LocalDateTime?): Collection<RefElement?>?
+  fun findAllByRefCategoryCodeAndBetweenStartAndEndDate(
+    refCategoryCode: String?,
+    endDate: LocalDateTime?
+  ): Collection<RefElement?>?
+}
+
+class ReferenceDataRepositoryImpl constructor(@PersistenceContext var em: EntityManager) {
+
+  fun findFilteredReferenceData(
+    username: String,
+    area: String?,
+    team: String?,
+    assessor: String?,
+    offenderPk: Long?,
+    oasysSetPk: Long?,
+    assessmentType: String?,
+    section: String?,
+    fieldName: String?,
+    parentList: Map<String, String>? = emptyMap(),
+    stage: String? = "CONTINUE"
+  ): String {
+
+    val session = em.unwrap(Session::class.java)
+
+    val query =
+      """DECLARE 
+            |LV_RES VARCHAR2(4000); 
+            |BEGIN 
+            |LV_RES := ARN_RESTFUL_API_PKG.refdata( 
+            |p_user => ?,
+            |p_area => ?, 
+            |p_team => ?, 
+            |p_assessor => ?, 
+            |p_offender_pk=> ?, 
+            |p_oasys_set_pk => ?, 
+            |p_assessment_type => ?, 
+            |p_stage => ?, 
+            |p_section => ?, 
+            |p_fieldname => ?, 
+            |p_parent_list => ?, 
+            |p_parent_values => ?);
+            |? := LV_RES; END;""".trimMargin()
+
+    return session.doReturningWork { connection: Connection ->
+      connection.prepareCall(query).use { function ->
+        function.setString(1, username)
+        function.setString(2, area)
+        function.setString(3, team)
+        function.setString(4, assessor)
+        if (offenderPk == null) function.setNull(5, Types.BIGINT) else function.setLong(5, offenderPk)
+        if (oasysSetPk == null) function.setNull(6, Types.BIGINT) else function.setLong(6, oasysSetPk)
+        function.setString(7, assessmentType)
+        function.setString(8, stage)
+        function.setString(9, section)
+        function.setString(10, fieldName)
+        function.setString(11, parentList?.keys?.joinToString(","))
+        function.setString(12, parentList?.values?.joinToString(","))
+        function.registerOutParameter(13, Types.VARCHAR)
+        function.execute()
+        return@doReturningWork function.getString(13)
+      }
+    }
+  }
 }
