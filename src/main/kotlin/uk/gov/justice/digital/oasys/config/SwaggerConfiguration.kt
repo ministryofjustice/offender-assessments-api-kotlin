a91b42502e18389b212ce65cacb0068995707c46
Stuart Whitlam <stuart.whitlam@digital.justice.gov.uk>
2021-06-25 15:00:16 +0100

    Update Swagger docs

---
+++ src/main/kotlin/uk/gov/justice/digital/oasys/config/SwaggerConfiguration.kt
@@ -0,0 +1,59 @@
+package uk.gov.justice.digital.oasys.config
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.info.BuildProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.Contact
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import java.time.LocalDateTime
+import java.time.ZonedDateTime
+import java.util.Date
+
+@Configuration
+class SwaggerConfiguration {
+  @Autowired(required = false)
+  private val buildProperties: BuildProperties? = null
+
+  @Bean
+  fun api(): Docket {
+    val apiInfo = ApiInfo(
+      "HMPPS Offender Assessment API",
+      "OASys Data API.",
+      version,
+      "",
+      contactInfo(),
+      "",
+      "",
+      emptyList()
+    )
+    val docket = Docket(DocumentationType.OAS_30)
+      .useDefaultResponseMessages(false)
+      .apiInfo(apiInfo)
+      .select()
+      .apis(RequestHandlerSelectors.basePackage("uk.gov.justice.digital.oasys"))
+      .paths(PathSelectors.any())
+      .build()
+    docket.directModelSubstitute(ZonedDateTime::class.java, Date::class.java)
+    docket.directModelSubstitute(LocalDateTime::class.java, Date::class.java)
+    return docket
+  }
+
+  /**
+   * @return health data. Note this is unsecured so no sensitive data allowed!
+   */
+  private val version: String
+    get() = if (buildProperties == null) "version not available" else buildProperties.version
+
+  private fun contactInfo(): Contact {
+    return Contact(
+      "HMPPS Digital Studio",
+      "",
+      "feedback@digital.justice.gov.uk"
+    )
+  }
+}
